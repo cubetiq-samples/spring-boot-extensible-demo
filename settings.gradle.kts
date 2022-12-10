fbe9c4b469b653597fd9d674e902dafa3101a595
Sambo Chea <sombochea@cubetiqs.com>
2022-12-10 09:47:35 +0700

    Updated

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "spring-function-custom-demo"
+rootProject.name = "spring-boot-extensible-demo"
 
 include("modules:plugin-context", "modules:spring-plugin-context")
 include("modules:my-plugin-1", "modules:my-plugin-2", "modules:my-plugin-3")
